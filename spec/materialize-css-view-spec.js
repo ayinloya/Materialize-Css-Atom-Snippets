97adbf09746917f589a5b37810e301c8f23ea9a1
Barnabas A Nsoh <banasco@gmail.com>
2016-10-02 03:08:31 +0000

    Start plugin for Atom

---
+++ spec/materialize-css-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MaterializeCssView from '../lib/materialize-css-view';
+
+describe('MaterializeCssView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
